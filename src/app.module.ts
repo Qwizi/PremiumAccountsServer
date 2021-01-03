b1549b4c2f3079f94e3d1932ceb7819239845604
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-03 17:42:26 +0100

    Added .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
