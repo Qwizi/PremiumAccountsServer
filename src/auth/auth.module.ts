20dbecbdacd781d248702908f8fc80b5c8f08771
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-03 17:46:07 +0100

    Added jwt tokens configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import {UsersModule} from "../users/users.module";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: {expiresIn: '3600s'}
+        })
+    ]
+})
 export class AuthModule {}
