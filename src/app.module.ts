ec2e849203d1cf5eaffac9481b208fc8819aff84
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-03 19:03:59 +0100

    Added forums module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import {ConfigModule} from "@nestjs/config";
+import { ForumsModule } from './forums/forums.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, UsersModule, AuthModule, ForumsModule],
   controllers: [AppController],
   providers: [AppService],
 })
