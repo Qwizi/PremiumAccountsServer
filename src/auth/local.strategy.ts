5cb5c6dd15a9a5a332976a36e6a3ada567789560
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-08 13:48:11 +0100

    PA-93 changed invalid login data message and response status

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import {Strategy} from "passport-local";
 import {PassportStrategy} from "@nestjs/passport";
-import {Injectable, UnauthorizedException} from "@nestjs/common";
+import {Injectable, NotFoundException} from "@nestjs/common";
 import {AuthService} from "./auth.service";
 
 @Injectable()
@@ -11,7 +11,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
     async validate(username: string, passport: string): Promise<any> {
         const user = await this.authService.validateUser({username: username, password: passport});
-        if (!user) throw new UnauthorizedException('username or passport is invalid')
+        if (!user) throw new NotFoundException('Username or password is invalid')
         return user;
     }
 }
