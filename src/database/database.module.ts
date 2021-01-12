f6a35a57c5d7643765f0cfa456849493d7eb3f43
Adrian Ciołek <qwizi95@protonmail.com>
2021-01-12 13:16:00 +0100

    PA-115 configured typeorm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import {databaseProviders} from "./database.providers";
 import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-    imports: [ConfigModule.forRoot()],
-    providers: [...databaseProviders],
-    exports: [...databaseProviders]
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: 3306,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_DATABASE,
+            entities: [],
+            synchronize: true
+        })
+    ],
+    //providers: [...databaseProviders],
+    //exports: [...databaseProviders]
 })
 export class DatabaseModule {}
